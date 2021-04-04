7b15f6a31b2570c30d13c8d8c6bb72d5401b391e
alifele <ali.fele@gmail.com>
2021-04-04 18:39:56 +0430

    update on the firmware

---
+++ LED Reactor Firmware/Core/Inc/LEDs.h
@@ -0,0 +1,23 @@
+#include "stm32f4xx_hal.h"
+typedef struct{
+
+	GPIO_TypeDef * RedLED_Port;
+	GPIO_TypeDef * WhiteLED_Port;
+	GPIO_TypeDef * GreenLED_Port;
+	GPIO_TypeDef * BlueLED_Port;
+	GPIO_TypeDef * OrangeLED_Port;
+
+
+	uint16_t RedLED_Pin
+	uint16_t GreeLED_Pin
+	uint16_t BlueLED_Pin;
+	uint16_t OrangeLED_Pin;
+	uint16_t WhiteLED_Pin;
+
+
+} LEDsConf_TypeDef;
+
+
+void LEDs_ConfigLEDs(LEDsConf_TypeDef * InitLEDs);
+void LEDs_TurnOnLED(int color);
+void LEDs_TurnOffLED(int color);
---
+++ LED Reactor Firmware/Core/Src/LEDs.c
@@ -0,0 +1,57 @@
+#include "LEDs.h"
+
+LEDsConf_TypeDef * LEDsConf;
+
+
+void LEDs_ConfigLEDs(LEDsConf_TypeDef * InitLEDs){
+	LEDsConf = InitLEDs;
+	HAL_GPIO_WritePin(InitLEDs->RedLED_Port, InitLEDs->RedLED_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(InitLEDs->GreenLED_Port, InitLEDs->GreenLED_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(InitLEDs->BlueLED_Port, InitLEDs->BlueLED_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(InitLEDs->OrangeLED_Port, InitLEDs->OrangeLED_Pin, REGPIO_PIN_RESETSET);
+	HAL_GPIO_WritePin(InitLEDs->WhiteLED_Port, InitLEDs->WhiteLED_Pin, GPIO_PIN_RESET);
+
+}
+
+
+void LEDs_TurnOnLED(int color){
+	//  1-> R, 2-> G, 3-> B, 4-> O, 5-> W
+
+	if (color == 1){
+			HAL_GPIO_WritePin(LEDsConf->RedLED_Port, LEDsConf->RedLED_Pin, GPIO_PIN_SET);
+	}
+	if (color == 2){
+			HAL_GPIO_WritePin(LEDsConf->GreenLED_Port, LEDsConf->GreenLED_Pin, GPIO_PIN_SET);
+	}
+	if (color == 3){
+			HAL_GPIO_WritePin(LEDsConf->BlueLED_Port, LEDsConf->BlueLED_Pin, GPIO_PIN_SET);
+	}
+	if (color == 4){
+			HAL_GPIO_WritePin(LEDsConf->OrangeLED_Port, LEDsConf->OrangeLED_Pin, GPIO_PIN_SET);
+	}
+	if (color == 5){
+			HAL_GPIO_WritePin(LEDsConf->WhiteLED_Port, LEDsConf->WhiteLED_Pin, GPIO_PIN_SET);
+	}
+
+}
+
+void LEDs_TurnOffLED(int color){
+	//  1-> R, 2-> G, 3-> B, 4-> O, 5-> W
+
+	if (color == 1){
+			HAL_GPIO_WritePin(LEDsConf->RedLED_Port, LEDsConf->RedLED_Pin, GPIO_PIN_RESET);
+	}
+	if (color == 2){
+			HAL_GPIO_WritePin(LEDsConf->GreenLED_Port, LEDsConf->GreenLED_Pin, GPIO_PIN_RESET);
+	}
+	if (color == 3){
+			HAL_GPIO_WritePin(LEDsConf->BlueLED_Port, LEDsConf->BlueLED_Pin, GPIO_PIN_RESET);
+	}
+	if (color == 4){
+			HAL_GPIO_WritePin(LEDsConf->OrangeLED_Port, LEDsConf->OrangeLED_Pin, GPIO_PIN_RESET);
+	}
+	if (color == 5){
+			HAL_GPIO_WritePin(LEDsConf->WhiteLED_Port, LEDsConf->WhiteLED_Pin, GPIO_PIN_RESET);
+	}
+
+}
